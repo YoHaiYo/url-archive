288e907feac3e3abc7ea19806dbc81c39430aa56
YoHaiYo <12si47bun@naver.com>
2024-07-21 14:42:37 +0900

    NavBar 컴포넌트화

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,12 @@
 <template>
-  <nav class="text-end">
-    <p class="text-4xl font-bold text-blue-500">테일윈드</p>
-    <router-link to="/">home</router-link>
-    <!-- <router-link to="/authtest">authtest</router-link> -->
-    <router-link v-if="!user" to="/login">login</router-link>
-    <router-link v-if="!user" to="/signup">signup</router-link>
-    <span v-if="user">Hello, {{ user.email }}</span>
-    <router-link v-if="user" to="/notes">dashboard</router-link>
-    <button v-if="user" @click="logout">LogOut</button>
-  </nav>
-  <hr />
+  <!-- 네비바 -->
+  <NavBar />
+  <!--- 메인 -->
   <router-view></router-view>
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
-import { supabase } from "../util/supabase/supabase";
-import { logout } from "../util/supabase/authUtils";
-import { getSessionData } from "../util/supabase/authUtils";
-
-const user = ref(null);
-
-onMounted(async () => {
-  user.value = await getSessionData();
-});
+import NavBar from "./components/NavBar.vue";
 </script>
 
-<style scoped lang="scss">
-ul {
-  list-style: none;
-}
-nav {
-  a {
-    margin: 0 10px;
-  }
-}
-/* common */
-.bold {
-  font-weight: bold;
-}
-.text-end {
-  text-align: end;
-}
-</style>
+<style scoped lang="scss"></style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,205 @@
+<!--
+md: 기준으로 반응형 처리함. 
+-->
+<template>
+  <!--- PC뷰 -->
+  <nav class="hidden md:flex justify-between p-6 px-4">
+    <div class="flex justify-between items-center w-full">
+      <!-- 로고 -->
+      <div class="md:w-1/3">
+        <router-link class="block max-w-max" to="/">
+          <img
+            class="h-8"
+            src="../assets/svg/flex-ui-violet-light.svg"
+            alt=""
+          />
+        </router-link>
+      </div>
+      <!-- 네비메뉴 -->
+      <div class="md:block md:w-1/3">
+        <ul class="flex justify-center">
+          <li class="mr-12">
+            <a
+              class="text-coolGray-500 hover:text-coolGray-900 font-medium"
+              href="#"
+            >
+              Product
+            </a>
+          </li>
+          <li class="mr-12">
+            <a
+              class="text-coolGray-500 hover:text-coolGray-900 font-medium"
+              href="#"
+            >
+              Features
+            </a>
+          </li>
+          <li class="mr-12">
+            <a
+              class="text-coolGray-500 hover:text-coolGray-900 font-medium"
+              href="#"
+            >
+              Resources
+            </a>
+          </li>
+          <li>
+            <a
+              class="text-coolGray-500 hover:text-coolGray-900 font-medium"
+              href="#"
+            >
+              Pricing
+            </a>
+          </li>
+        </ul>
+      </div>
+      <!-- 로그인 -->
+      <div class="md:block md:w-1/3">
+        <div class="flex items-center justify-end">
+          <div v-if="!user" class="flex items-center gap-4">
+            <router-link
+              class="inline-block py-2 px-4 text-sm leading-5 text-violet-50 bg-violet-500 hover:bg-violet-600 font-medium focus:ring-2 focus:ring-violet-500 focus:ring-opacity-50 rounded-md"
+              to="/login"
+              >Login</router-link
+            >
+            <router-link to="/signup">Sign Up</router-link>
+          </div>
+          <div v-if="user" class="flex items-center gap-4">
+            <span>Hello, {{ user.email }} !</span>
+            <router-link
+              class="inline-block py-2 px-4 text-sm leading-5 text-violet-50 bg-violet-500 hover:bg-violet-600 font-medium focus:ring-2 focus:ring-violet-500 focus:ring-opacity-50 rounded-md"
+              to="/notes"
+              >dashboard</router-link
+            >
+            <button
+              class="inline-block py-2 px-4 mr-2 leading-5 text-coolGray-500 hover:text-coolGray-900 bg-transparent font-medium rounded-md"
+              @click="logout"
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <!--- 모바일뷰 -->
+  <div
+    class="navbar-menu md:hidden fixed top-0 left-0 z-50 w-full h-full bg-coolGray-900 bg-opacity-50"
+  >
+    <div class="fixed top-0 left-0 bottom-0 w-full w-4/6 max-w-xs bg-white">
+      <nav class="relative p-6 h-full overflow-y-auto">
+        <div class="flex flex-col justify-between h-full">
+          <a class="inline-block" href="#">
+            <img
+              class="h-8"
+              src="../assets/svg/flex-ui-violet-light.svg"
+              alt=""
+            />
+          </a>
+          <ul class="py-6">
+            <li>
+              <a
+                class="block py-3 px-4 text-coolGray-500 hover:text-coolGray-900 font-medium hover:bg-coolGray-50 rounded-md"
+                href="#"
+              >
+                Product
+              </a>
+            </li>
+            <li>
+              <a
+                class="block py-3 px-4 text-coolGray-500 hover:text-coolGray-900 font-medium hover:bg-coolGray-50 rounded-md"
+                href="#"
+              >
+                Features
+              </a>
+            </li>
+            <li>
+              <a
+                class="block py-3 px-4 text-coolGray-500 hover:text-coolGray-900 font-medium hover:bg-coolGray-50 rounded-md"
+                href="#"
+              >
+                Pricing
+              </a>
+            </li>
+            <li>
+              <a
+                class="block py-3 px-4 text-coolGray-500 hover:text-coolGray-900 font-medium hover:bg-coolGray-50 rounded-md"
+                href="#"
+              >
+                Resources
+              </a>
+            </li>
+          </ul>
+          <div class="flex flex-wrap">
+            <div class="w-full mb-2">
+              <router-link
+                class="mb-2 inline-block py-2 px-4 w-full text-sm leading-5 text-coolGray-500 hover:text-coolGray-900 bg-transparent font-medium text-center rounded-md"
+                v-if="!user"
+                to="/login"
+                >Login</router-link
+              >
+            </div>
+            <div class="w-full">
+              <router-link
+                class="inline-block py-2 px-4 w-full text-sm leading-5 text-white bg-violet-500 hover:bg-violet-600 font-medium text-center focus:ring-2 focus:ring-violet-500 focus:ring-opacity-50 rounded-md"
+                v-if="!user"
+                to="/signup"
+                >Sign Up</router-link
+              >
+            </div>
+            <span
+              class="mb-2 inline-block py-2 px-4 w-full text-sm leading-5 text-coolGray-500 hover:text-coolGray-900 bg-transparent font-medium text-center rounded-md"
+              v-if="user"
+              >Hello, {{ user.email }}</span
+            >
+            <div class="w-full mb-2">
+              <router-link
+                class="inline-block py-2 px-4 w-full text-sm leading-5 text-white bg-violet-500 hover:bg-violet-600 font-medium text-center focus:ring-2 focus:ring-violet-500 focus:ring-opacity-50 rounded-md"
+                v-if="user"
+                to="/notes"
+                >dashboard</router-link
+              >
+            </div>
+            <div class="w-full mb-2">
+              <a
+                class="inline-block py-2 px-4 w-full text-sm leading-5 text-white bg-violet-500 hover:bg-violet-600 font-medium text-center focus:ring-2 focus:ring-violet-500 focus:ring-opacity-50 rounded-md"
+                v-if="user"
+                @click="logout"
+              >
+                LogOut
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <a class="navbar-close absolute top-5 p-4 right-3" href="#">
+        <svg
+          width="12"
+          height="12"
+          viewBox="0 0 12 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M6.94004 6L11.14 1.80667C11.2656 1.68113 11.3361 1.51087 11.3361 1.33333C11.3361 1.1558 11.2656 0.985537 11.14 0.860002C11.0145 0.734466 10.8442 0.66394 10.6667 0.66394C10.4892 0.66394 10.3189 0.734466 10.1934 0.860002L6.00004 5.06L1.80671 0.860002C1.68117 0.734466 1.51091 0.663941 1.33337 0.663941C1.15584 0.663941 0.985576 0.734466 0.860041 0.860002C0.734505 0.985537 0.66398 1.1558 0.66398 1.33333C0.66398 1.51087 0.734505 1.68113 0.860041 1.80667L5.06004 6L0.860041 10.1933C0.797555 10.2553 0.747959 10.329 0.714113 10.4103C0.680267 10.4915 0.662842 10.5787 0.662842 10.6667C0.662842 10.7547 0.680267 10.8418 0.714113 10.9231C0.747959 11.0043 0.797555 11.078 0.860041 11.14C0.922016 11.2025 0.99575 11.2521 1.07699 11.2859C1.15823 11.3198 1.24537 11.3372 1.33337 11.3372C1.42138 11.3372 1.50852 11.3198 1.58976 11.2859C1.671 11.2521 1.74473 11.2025 1.80671 11.14L6.00004 6.94L10.1934 11.14C10.2554 11.2025 10.3291 11.2521 10.4103 11.2859C10.4916 11.3198 10.5787 11.3372 10.6667 11.3372C10.7547 11.3372 10.8419 11.3198 10.9231 11.2859C11.0043 11.2521 11.0781 11.2025 11.14 11.14C11.2025 11.078 11.2521 11.0043 11.286 10.9231C11.3198 10.8418 11.3372 10.7547 11.3372 10.6667C11.3372 10.5787 11.3198 10.4915 11.286 10.4103C11.2521 10.329 11.2025 10.2553 11.14 10.1933L6.94004 6Z"
+            fill="#556987"
+          />
+        </svg>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import { supabase } from "../../util/supabase/supabase";
+import { logout } from "../../util/supabase/authUtils";
+import { getSessionData } from "../../util/supabase/authUtils";
+
+const user = ref(null);
+
+onMounted(async () => {
+  user.value = await getSessionData();
+});
+</script>
+
+<style scoped lang="scss"></style>
